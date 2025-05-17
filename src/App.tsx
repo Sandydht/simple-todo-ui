fe5557068441336928e36758a3618a30c8bad1e8
sandy <sandydwi75@gmail.com>
2025-05-17 10:52:06 +0700

    feat: finish slicing

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ const App = () => {
         <div className="w-full h-auto flex flex-col items-start justify-start gap-[24px] rounded-[16px] bg-white p-[25px] shadow-lg border-[1px] border-gray-400">
           <div className="w-full h-auto">
             <p className="text-left text-[24px] leading-[32px] text-[#000000] font-semibold">
-              Task List
+              Simple TODO
             </p>
           </div>
 
