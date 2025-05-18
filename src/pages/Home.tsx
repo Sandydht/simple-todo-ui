f4361e49cc7beb24b9c2fe315ad2ae9fe98b9690
sandy <sandydwi75@gmail.com>
2025-05-18 12:19:55 +0700

    feat: finish task detail

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,24 +10,28 @@ import { hideConfirmationModalBox, setConfirmationModalBoxLoading, showConfirmat
 import { useHistory } from "react-router-dom";
 import TaskFormModal from "../components/TaskFormModal";
 import { getTaskList } from "../services/task.service";
+import TaskDetailModal from "../components/TaskDetailModal";
+
+export interface TaskData {
+  id: number;
+  title: string;
+  description: string;
+  start_date: string;
+  end_date: string;
+  label_color: string;
+  is_done: boolean;
+  created_at: string;
+  updated_at: string;
+}
 
 const Home = () => {
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isShowTaskFormModal, setIsShowTaskFormModal] = useState<boolean>(false);
   const [isShowTaskDetailModal, setIsShowTaskDetailModal] = useState<boolean>(false);
-  const [taskData, setTaskData] = useState<{
-    id: number;
-    title: string;
-    description: string;
-    start_date: string;
-    end_date: string;
-    label_color: string;
-    is_done: boolean;
-    created_at: string;
-    updated_at: string;
-  }[]>([])
+  const [taskData, setTaskData] = useState<TaskData[]>([])
   const history = useHistory();
+  const [selectedTask, setSelectedTask] = useState<TaskData | null>(null);
 
   useEffect(() => {
     fetchUserProfileData()
@@ -63,7 +67,8 @@ const Home = () => {
 
   const handleClickDetail = (taskId: number) => {
     setIsShowTaskDetailModal(true);
-    console.log('taskId: ', taskId)
+    const task = findTask(taskId);
+    setSelectedTask(task);
   }
 
   const handleLogout = () => {
@@ -120,6 +125,12 @@ const Home = () => {
     setIsShowTaskFormModal(false)
   }
 
+  const findTask = (taskId: number): TaskData | null => {
+    if (taskData.length < 1) return null;
+    const task = taskData.find((item: TaskData) => item.id == taskId);
+    return task || null;
+  }
+
   return (
     <>
       {isShowTaskFormModal && (
@@ -129,8 +140,15 @@ const Home = () => {
         />
       )}
 
+      {isShowTaskDetailModal && (
+        <TaskDetailModal
+          taskData={selectedTask}
+          onClose={() => setIsShowTaskDetailModal(false)}
+        />
+      )}
+
       <div className="w-full h-auto min-h-screen bg-linear-to-t from-sky-500 to-indigo-500">
-        <div className="w-full h-auto bg-white flex items-center justify-center px-[20px] py-[10px] shadow-lg">
+        <div className="w-full h-auto bg-white flex items-center justify-center px-[20px] py-[10px] shadow-lg sticky top-0 z-10">
           <div className="w-full h-auto max-w-[1024px] flex items-center justify-between px-[20px] py-[10px]">
             <p className="text-left text-[24px] leading-[32px] text-[#000000] font-semibold">
               Simple TODO
@@ -150,25 +168,29 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="w-full h-auto flex flex-col items-start justify-start gap-[25px] max-w-[728px] mx-auto p-[25px]">
-          <div className="w-full h-auto flex flex-col items-start justify-start gap-[24px] rounded-[16px] bg-white p-[25px] shadow-lg border-[1px] border-gray-400">
-            <div className="w-full h-auto">
-              <p className="text-left text-[24px] leading-[32px] text-[#000000] font-semibold">
-                Task List
-              </p>
-            </div>
+        {!isLoading && (
+          <>
+            <div className="w-full h-auto flex flex-col items-start justify-start gap-[25px] max-w-[728px] mx-auto p-[25px]">
+              <div className="w-full h-auto flex flex-col items-start justify-start gap-[24px] rounded-[16px] bg-white p-[25px] shadow-lg border-[1px] border-gray-400">
+                <div className="w-full h-auto">
+                  <p className="text-left text-[24px] leading-[32px] text-[#000000] font-semibold">
+                    Task List
+                  </p>
+                </div>
 
-            <div className="w-full h-auto flex flex-col items-start justify-start gap-[16px]">
-              {taskData.map((item) => (
-                <TaskItem
-                  key={item.id}
-                  data={item}
-                  onClick={handleClickDetail}
-                />
-              ))}
+                <div className="w-full h-auto flex flex-col items-start justify-start gap-[16px]">
+                  {taskData.map((item) => (
+                    <TaskItem
+                      key={item.id}
+                      data={item}
+                      onClick={handleClickDetail}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
 
         <button
           type="button"
